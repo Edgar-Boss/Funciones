47e91d92d4b162f541d2cd9ef55ba9756b361ed7
Edgar-Boss <edgardiazortiz@gmail.com>
2018-01-14 23:46:30 -0600

    1.0
    solo evalua funciones sencillas (2x+1)

---
+++ validador de funciones/validador de funciones/Form1.Designer.cs
@@ -0,0 +1,212 @@
+﻿namespace validador_de_funciones
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.bunifuElipse1 = new Bunifu.Framework.UI.BunifuElipse(this.components);
+            this.bunifuDragControl1 = new Bunifu.Framework.UI.BunifuDragControl(this.components);
+            this.Header = new System.Windows.Forms.Panel();
+            this.bunifuImageButton1 = new Bunifu.Framework.UI.BunifuImageButton();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.Iniciar = new Bunifu.Framework.UI.BunifuThinButton2();
+            this.label1 = new System.Windows.Forms.Label();
+            this.f = new System.Windows.Forms.TextBox();
+            this.bunifuCustomLabel2 = new Bunifu.Framework.UI.BunifuCustomLabel();
+            this.funcion = new System.Windows.Forms.TextBox();
+            this.bunifuCustomLabel1 = new Bunifu.Framework.UI.BunifuCustomLabel();
+            this.Header.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.bunifuImageButton1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // bunifuElipse1
+            // 
+            this.bunifuElipse1.ElipseRadius = 5;
+            this.bunifuElipse1.TargetControl = this;
+            // 
+            // bunifuDragControl1
+            // 
+            this.bunifuDragControl1.Fixed = true;
+            this.bunifuDragControl1.Horizontal = true;
+            this.bunifuDragControl1.TargetControl = this.Header;
+            this.bunifuDragControl1.Vertical = true;
+            // 
+            // Header
+            // 
+            this.Header.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(102)))), ((int)(((byte)(204)))));
+            this.Header.Controls.Add(this.bunifuImageButton1);
+            this.Header.Dock = System.Windows.Forms.DockStyle.Top;
+            this.Header.Location = new System.Drawing.Point(0, 0);
+            this.Header.Name = "Header";
+            this.Header.Size = new System.Drawing.Size(394, 32);
+            this.Header.TabIndex = 0;
+            // 
+            // bunifuImageButton1
+            // 
+            this.bunifuImageButton1.BackColor = System.Drawing.Color.SeaGreen;
+            this.bunifuImageButton1.Image = ((System.Drawing.Image)(resources.GetObject("bunifuImageButton1.Image")));
+            this.bunifuImageButton1.ImageActive = null;
+            this.bunifuImageButton1.Location = new System.Drawing.Point(344, 0);
+            this.bunifuImageButton1.Name = "bunifuImageButton1";
+            this.bunifuImageButton1.Size = new System.Drawing.Size(47, 26);
+            this.bunifuImageButton1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.bunifuImageButton1.TabIndex = 0;
+            this.bunifuImageButton1.TabStop = false;
+            this.bunifuImageButton1.Zoom = 10;
+            this.bunifuImageButton1.Click += new System.EventHandler(this.bunifuImageButton1_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(26)))), ((int)(((byte)(32)))), ((int)(((byte)(40)))));
+            this.panel1.Controls.Add(this.Iniciar);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.f);
+            this.panel1.Controls.Add(this.bunifuCustomLabel2);
+            this.panel1.Controls.Add(this.funcion);
+            this.panel1.Controls.Add(this.bunifuCustomLabel1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.panel1.Location = new System.Drawing.Point(0, 32);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(394, 222);
+            this.panel1.TabIndex = 1;
+            // 
+            // Iniciar
+            // 
+            this.Iniciar.ActiveBorderThickness = 1;
+            this.Iniciar.ActiveCornerRadius = 20;
+            this.Iniciar.ActiveFillColor = System.Drawing.Color.SeaGreen;
+            this.Iniciar.ActiveForecolor = System.Drawing.Color.White;
+            this.Iniciar.ActiveLineColor = System.Drawing.Color.SeaGreen;
+            this.Iniciar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(26)))), ((int)(((byte)(32)))), ((int)(((byte)(40)))));
+            this.Iniciar.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("Iniciar.BackgroundImage")));
+            this.Iniciar.ButtonText = "ThinButton";
+            this.Iniciar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.Iniciar.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.Iniciar.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Iniciar.ForeColor = System.Drawing.Color.SeaGreen;
+            this.Iniciar.IdleBorderThickness = 1;
+            this.Iniciar.IdleCornerRadius = 10;
+            this.Iniciar.IdleFillColor = System.Drawing.Color.White;
+            this.Iniciar.IdleForecolor = System.Drawing.Color.SeaGreen;
+            this.Iniciar.IdleLineColor = System.Drawing.Color.SeaGreen;
+            this.Iniciar.Location = new System.Drawing.Point(0, 181);
+            this.Iniciar.Margin = new System.Windows.Forms.Padding(5);
+            this.Iniciar.Name = "Iniciar";
+            this.Iniciar.Size = new System.Drawing.Size(394, 41);
+            this.Iniciar.TabIndex = 5;
+            this.Iniciar.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.Iniciar.Click += new System.EventHandler(this.Iniciar_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Location = new System.Drawing.Point(240, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(13, 18);
+            this.label1.TabIndex = 4;
+            this.label1.Text = ")";
+            // 
+            // f
+            // 
+            this.f.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.f.Location = new System.Drawing.Point(206, 31);
+            this.f.Name = "f";
+            this.f.Size = new System.Drawing.Size(36, 20);
+            this.f.TabIndex = 3;
+            this.f.Text = "4";
+            // 
+            // bunifuCustomLabel2
+            // 
+            this.bunifuCustomLabel2.AutoSize = true;
+            this.bunifuCustomLabel2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bunifuCustomLabel2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.bunifuCustomLabel2.Location = new System.Drawing.Point(190, 30);
+            this.bunifuCustomLabel2.Name = "bunifuCustomLabel2";
+            this.bunifuCustomLabel2.Size = new System.Drawing.Size(17, 18);
+            this.bunifuCustomLabel2.TabIndex = 2;
+            this.bunifuCustomLabel2.Text = "f(";
+            // 
+            // funcion
+            // 
+            this.funcion.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.funcion.Location = new System.Drawing.Point(62, 31);
+            this.funcion.Name = "funcion";
+            this.funcion.Size = new System.Drawing.Size(119, 20);
+            this.funcion.TabIndex = 1;
+            this.funcion.Text = "3x^2-2x+1";
+            // 
+            // bunifuCustomLabel1
+            // 
+            this.bunifuCustomLabel1.AutoSize = true;
+            this.bunifuCustomLabel1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bunifuCustomLabel1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.bunifuCustomLabel1.Location = new System.Drawing.Point(25, 31);
+            this.bunifuCustomLabel1.Name = "bunifuCustomLabel1";
+            this.bunifuCustomLabel1.Size = new System.Drawing.Size(38, 18);
+            this.bunifuCustomLabel1.TabIndex = 0;
+            this.bunifuCustomLabel1.Text = "f(x)=";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(394, 254);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.Header);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Header.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.bunifuImageButton1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Bunifu.Framework.UI.BunifuElipse bunifuElipse1;
+        private System.Windows.Forms.Panel Header;
+        private Bunifu.Framework.UI.BunifuDragControl bunifuDragControl1;
+        private System.Windows.Forms.Panel panel1;
+        private Bunifu.Framework.UI.BunifuCustomLabel bunifuCustomLabel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox f;
+        private Bunifu.Framework.UI.BunifuCustomLabel bunifuCustomLabel2;
+        private System.Windows.Forms.TextBox funcion;
+        private Bunifu.Framework.UI.BunifuThinButton2 Iniciar;
+        private Bunifu.Framework.UI.BunifuImageButton bunifuImageButton1;
+    }
+}
+
---
+++ validador de funciones/validador de funciones/Form1.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace validador_de_funciones
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        string[] elementos_funcion= new string[20];
+        char[] funcion_;
+        int[] val_numeros = new int[20];
+        int f_;
+        int tope = 0;
+        private void Iniciar_Click(object sender, EventArgs e)
+        {
+
+            funcion_ = funcion.Text.ToCharArray();
+            f_ = Int16.Parse(f.Text);
+
+            Identifica_elemento();
+            
+            Realiza_operaciones();
+
+            Asigna_valor();
+
+            //for (int k = 0; k <= tope; k++)
+            //{
+            //    MessageBox.Show(elementos_funcion[k]);
+            //    MessageBox.Show(val_numeros[k].ToString());
+            //}
+
+        }
+
+
+        void Asigna_valor()
+        {
+            int acum = int.Parse(elementos_funcion[0]);
+            int ind_arr=0;
+            int[] arr = new int[20];
+            int res = 0;
+            for (int k=0;k<=tope;k++)
+            {
+               
+
+                if (elementos_funcion[k] == "+" || elementos_funcion[k] == "-")
+                {
+                    arr[ind_arr] = val_numeros[k] * val_numeros[k + 1];
+                    ind_arr++;
+                    k++;
+                }
+                else if(elementos_funcion[k] != "+" && elementos_funcion[k] != "-")
+                {
+                    
+                    
+
+                    arr[ind_arr] = val_numeros[k];
+                    ind_arr++;
+                }
+                
+            }
+
+            for (int k = 0; k < ind_arr; k++)
+            {
+                res = res+arr[k]  ;
+            }
+
+            MessageBox.Show(res.ToString());
+
+        }
+
+        void Realiza_operaciones()
+        {
+
+            char[] arraychar;
+            int res = 1;
+
+            int u_elemnto = 0;
+            bool ban = false;
+            for (int y=0;y<=tope;y++)
+            {
+               // MessageBox.Show(elementos_funcion[y]);
+                res = 1;
+                arraychar = elementos_funcion[y].ToCharArray();
+                if (elementos_funcion[y] != "+" && elementos_funcion[y] != "-")
+                {
+                    ban = true;
+                    for (int j = 0; j < arraychar.Length; j++)
+                    {
+                        if (arraychar[j] == 'x')
+                        {
+                            arraychar[j] =char.Parse(f_.ToString());
+                        }
+                        
+                        res = res * (int.Parse(arraychar[j].ToString()));
+                    }
+                   
+                }
+                if (ban == true)
+                {
+                    val_numeros[u_elemnto] = res;   
+                    elementos_funcion[u_elemnto] = res.ToString();
+                    u_elemnto++;
+                    ban = false;
+                }
+                else
+                {
+                    elementos_funcion[u_elemnto] = elementos_funcion[y];
+                   
+
+
+                    if (elementos_funcion[y] == "+")
+                    {
+                        val_numeros[u_elemnto] = 1;
+                    }
+                    if (elementos_funcion[y] == "-")
+                    {
+                        val_numeros[u_elemnto] = -1;
+                    }
+                    u_elemnto++;
+                }
+
+            }
+
+        }
+
+        void Identifica_elemento()
+        {
+            
+            elementos_funcion[tope] = string.Empty;
+            
+            for (int k = 0; k < funcion_.Length; k++)
+            {
+                if (funcion_[k] != '+' && funcion_[k] != '-')
+                {
+                    elementos_funcion[tope]= elementos_funcion[tope] + funcion_[k].ToString();
+
+                    
+                }
+                else //if (funcion_[k] == '+' || funcion_[k] == '-')
+                {
+
+                    tope++;
+                    elementos_funcion[tope] = funcion_[k].ToString();
+                    tope++;
+                }
+                
+            }
+            for (int k = 0; k <=tope; k++)
+            {
+                
+                MessageBox.Show(elementos_funcion[k]);
+            }
+
+
+        }
+        private void bunifuImageButton1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
